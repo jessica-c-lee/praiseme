2f91ffaca6c10c5a7417e8efe57434c63692fed1
jessica-c-lee <jesssica.lee88@gmail.com>
2019-12-13 14:46:06 +1100

    fixed compliments

--- R/praise.R
+++ R/praise.R
@@ -8,9 +8,9 @@
 #' @export
 #' @examples praise(name = "Jess", punctuation = "!)
 praise <- function(name = "Jess", punctuation = "!", PG = TRUE) {
-  compliments_PG <- c(glue::glue("You're a motherfucking badass, {name}{punctuation}"),
+  compliments <- c(glue::glue("You're a motherfucking badass, {name}{punctuation}"),
                    glue::glue("Code like a badass, {name}{punctuation}"))
-  compliments <- c(glue::glue("Today will be productive, {name}{punctuation}"),
+  compliments_PG <- c(glue::glue("Today will be productive, {name}{punctuation}"),
                       glue::glue("You're killing it, {name}{punctuation}"),
                       glue::glue("You're the best, {name}{punctuation}"))
   if (PG == TRUE) {
