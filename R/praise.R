b47a6bf44e4954cde00632c6d2aab2c3f9751b0f
jessica-c-lee <jesssica.lee88@gmail.com>
2019-12-13 14:40:44 +1100

    add PG and nonPG compliments

--- R/praise.R
+++ R/praise.R
@@ -7,9 +7,15 @@
 #' @return Text string with praise
 #' @export
 #' @examples praise(name = "Jess", punctuation = "!)
-praise <- function(name = "Jess", punctuation = "!") {
-  compliments <- c(glue::glue("You're a badass, {name}{punctuation}"),
-                   glue::glue("Code like a badass, {name}{punctuation}"),
-                   glue::glue("You're the best, {name}{punctuation}"))
-  sample(compliments, 1)
+praise <- function(name = "Jess", punctuation = "!", PG = TRUE) {
+  compliments_PG <- c(glue::glue("You're a motherfucking badass, {name}{punctuation}"),
+                   glue::glue("Code like a badass, {name}{punctuation}"))
+  compliments <- c(glue::glue("Today will be productive, {name}{punctuation}"),
+                      glue::glue("You're killing it, {name}{punctuation}"),
+                      glue::glue("You're the best, {name}{punctuation}"))
+  if (PG == TRUE) {
+    sample(compliments_PG, 1)
+  } else if (PG == FALSE) {
+    sample(compliments, 1)
+  }
 }
